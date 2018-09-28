cfa17b2debfd6c3f291fbe2d25f835767f34e6eb
Thibault Duplessis <thibault.duplessis@gmail.com>
2018-09-28 11:53:36 +0200

    let the requester choose the chunk size

--- server.js
+++ server.js
@@ -1,5 +1,12 @@
+// run server:
 // node server.js <port> <source> <delay>
 // node server.js 9999 http://54.194.223.217/batumiolympiad2018/live/open-03.pgn 3
+
+// request games:
+// GET /<nbPerChunk>/<chunkId>.pgn
+// GET /64/1.pgn
+// GET /64/2.pgn
+
 const express = require('express')
 const request = require('request');
 
@@ -8,39 +15,25 @@ const source = process.argv[3];
 const delay = parseInt(process.argv[4]);
 const separator = '\n\n\n';
 
-let chunks = [];
+let allGames = [];
 
 function getFullPgn() {
   request(source, (err, res, body) => {
-    chunks = makeChunks(body);
-    console.log(`Made ${chunks.length} chunks of ${chunks.map(c => c.length).join(', ')} games.`);
-    scheduleGetFullPgn();
+    allGames = body.split(separator).filter(g => !!g);
+    console.log(`Got ${allGames.length} games (${body.length} bytes)`);
+    setTimeout(getFullPgn, delay * 1000);
   });
 }
-
-function scheduleGetFullPgn() {
-  setTimeout(getFullPgn, delay * 1000);
-}
-
-function makeChunks(full) {
-  const games = full.split(separator).filter(g => !!g);
-  return chunkArray(games, 64);
-}
-
-function chunkArray(arr, size){
-  let results = [];
-  while (arr.length) results.push(arr.splice(0, size));
-  return results;
-}
+getFullPgn();
 
 const app = express()
-app.get('/chunk/:id', function (req, res) {
-  const chunkId = parseInt(req.params.id) - 1;
-  const games = chunks[chunkId];
-  if (games) res.send(games.join(separator));
+app.get('/:nbPerChunk/:chunkId.pgn', function (req, res) {
+  const nbPerChunk = parseInt(req.params.nbPerChunk);
+  const chunkId = parseInt(req.params.chunkId) - 1;
+  const games = allGames.slice(chunkId * nbPerChunk, (chunkId + 1) * nbPerChunk);
+  if (games.length) res.send(games.join(separator));
   else res.status(404).end();
 })
 app.listen(port, function () {
-  console.log(`Server is listening on http://127.0.0.1/${port}/chunk/1`);
+  console.log(`Server is listening on port ${port}`);
 })
-getFullPgn();
